8321e350ad2388a7e5064fdd4d55f103bbbe0846
essien emmanuel <essienemma300@gmail.com>
2024-06-23 21:27:46 +0100

    fixed bug with the maze generation code

--- src/index.js
+++ src/index.js
@@ -9,11 +9,13 @@ const height = 600;
 const halfWidth = width / 2;
 const halfHeight = height / 2;
 
+const unitLength = width / cells;
+
 const render = Render.create({
   element: document.body,
   engine: engine,
   options: {
-    // wireframes: false,
+    wireframes: true,
     width: width,
     height: height
   }
@@ -44,7 +46,7 @@ const shuffle = (arr) => {
     arr[counter] = arr[index];
     arr[index] = temp;
   }
-
+  
   return arr
 }
 
@@ -58,18 +60,22 @@ const startRow = Math.floor(Math.random() * cells);
 
 const startColumn = Math.floor(Math.random() * cells);
 
-const stepThroughCells = (row, column) => {
+// /**
+//  * side note:: true in our matrix represents 'no wall'
+//  */
+
+const stepThroughCell = (row, column) => {
   // if cell at [row, column] visited, return true
   if (grid[row][column]) return
-
+  
   // Mark cell visited, that is to true
   grid[row][column] = true;
 
   // Assemble randomly-ordered list of Neighbors
   const upNeighbor = [row - 1, column, 'up'];
+  const rightNeighbor = [row, column + 1, 'right'];
   const downNeighbor = [row + 1, column, 'down'];
   const leftNeighbor = [row, column - 1, 'left'];
-  const rightNeighbor = [row, column + 1, 'right'];
 
   const neighbors = shuffle([
     upNeighbor,
@@ -87,7 +93,7 @@ const stepThroughCells = (row, column) => {
     if (nextRow < 0 || nextRow >= cells || nextColumn < 0 || nextColumn >= cells) continue;
 
     // if next neighbor, continue to next neighbor
-    if (grid[nextRow][nextColumn]) return;
+    if (grid[nextRow][nextColumn]) continue;
 
     // Remove a wall from horizontals or verticals array
 
@@ -105,9 +111,51 @@ const stepThroughCells = (row, column) => {
       horizontals[row][column] = true;
     }
 
-    stepThroughCells(nextRow, nextColumn)
+    // Visit that next cell
+    stepThroughCell(nextRow, nextColumn);
   }
 
-  // Visit that next cell
 }
-stepThroughCells(startRow, startColumn)
+
+stepThroughCell(startRow, startColumn);
+
+
+// // Drawing to the canvas
+
+const wallThickness = 5
+
+// Horizontal walls
+horizontals.forEach((row, rowIndex) => {
+  row.forEach((open, columnIndex) => {
+    if (open) return;
+
+    const x = (columnIndex * unitLength) + (unitLength / 2);
+    const y = (rowIndex * unitLength) + unitLength;
+    const wallWidth = unitLength;
+    const wallHeight = wallThickness;
+
+    const wall = Bodies.rectangle(x, y, wallWidth, wallHeight, {
+      isStatic: true
+    });
+
+    World.add(world, wall)
+  });
+});
+
+//vertical walls
+verticals.forEach((row, rowIndex) => {
+  row.forEach((open, columnIndex) => {
+    if (open) return;
+
+    const x = (columnIndex * unitLength) + unitLength;
+    const y = (rowIndex * unitLength) + (unitLength / 2);
+    const wallWidth = wallThickness;
+    const wallHeight = unitLength;
+    
+    const wall = Bodies.rectangle(x, y, wallWidth, wallHeight, {
+      isStatic: true
+    });
+
+    World.add(world, wall);
+  });
+});
