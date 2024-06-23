b58d4ec7bf02f7534df4623a42a047dec5e4dd5b
essien emmanuel <essienemma300@gmail.com>
2024-06-23 15:35:12 +0100

    maze generation function

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const { World, Engine, Runner, Render, Bodies } = Matter;
 const engine = Engine.create();
 const { world } = engine;
 
-const cells = 5;
+const cells = 3;
 const width = 600;
 const height = 600;
 const halfWidth = width / 2;
@@ -34,6 +34,20 @@ World.add(world, walls);
 
 // Maze generation
 
+const shuffle = (arr) => {
+  let counter = arr.length;
+
+  while (counter > 0) {
+    const index = Math.floor(Math.random() * counter);
+    counter--;
+    const temp = arr[counter];
+    arr[counter] = arr[index];
+    arr[index] = temp;
+  }
+
+  return arr
+}
+
 const grid = Array(cells).fill(null).map(() => Array(cells).fill(false))
 
 const verticals = Array(cells).fill(null).map(() => Array(cells - 1).fill(false));
@@ -45,6 +59,55 @@ const startRow = Math.floor(Math.random() * cells);
 const startColumn = Math.floor(Math.random() * cells);
 
 const stepThroughCells = (row, column) => {
+  // if cell at [row, column] visited, return true
+  if (grid[row][column]) return
 
+  // Mark cell visited, that is to true
+  grid[row][column] = true;
+
+  // Assemble randomly-ordered list of Neighbors
+  const upNeighbor = [row - 1, column, 'up'];
+  const downNeighbor = [row + 1, column, 'down'];
+  const leftNeighbor = [row, column - 1, 'left'];
+  const rightNeighbor = [row, column + 1, 'right'];
+
+  const neighbors = shuffle([
+    upNeighbor,
+    rightNeighbor,
+    downNeighbor,
+    leftNeighbor
+  ]);
+  
+  // for each neighbor
+
+  for (const neighbor of neighbors) { 
+    const [nextRow, nextColumn, direction] = neighbor;
+    
+    // check if neighbor is out of bound
+    if (nextRow < 0 || nextRow >= cells || nextColumn < 0 || nextColumn >= cells) continue;
+
+    // if next neighbor, continue to next neighbor
+    if (grid[nextRow][nextColumn]) return;
+
+    // Remove a wall from horizontals or verticals array
+
+    // For left and right walls (vertical arrays)
+    if (direction === 'left') {
+      verticals[row][column - 1] = true;
+    } else if (direction === 'right') {
+      verticals[row][column] = true;
+    }
+
+    // For up and down walls (horizontal arrays)
+    if (direction === 'up') {
+      horizontals[row -1][column] = true;
+    } else if (direction === 'down') {
+      horizontals[row][column] = true;
+    }
+
+    stepThroughCells(nextRow, nextColumn)
+  }
+
+  // Visit that next cell
 }
 stepThroughCells(startRow, startColumn)
