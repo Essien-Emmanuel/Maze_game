0debe6c8193d00a14056a54198026b8b53325074
essien emmanuel <essienemma300@gmail.com>
2024-06-24 07:18:45 +0100

    added code for screen responsiveness for width and height. also added a styled message for win condition

--- src/index.js
+++ src/index.js
@@ -4,19 +4,21 @@ const engine = Engine.create();
 const { world } = engine;
 engine.world.gravity.y = 0; //disable gravity in the y direction in the world
 
-const cells = 3;
-const width = 600;
-const height = 600;
+const cellsHorizontal = 14;
+const cellsVertical = 10;
+const width = window.innerWidth;
+const height = window.innerHeight;
 const halfWidth = width / 2;
 const halfHeight = height / 2;
 
-const unitLength = width / cells;
+const unitLengthX = width / cellsHorizontal;
+const unitLengthY = height / cellsVertical;
 
 const render = Render.create({
   element: document.body,
   engine: engine,
   options: {
-    wireframes: true,
+    wireframes: false,
     width: width,
     height: height
   }
@@ -51,15 +53,18 @@ const shuffle = (arr) => {
   return arr
 }
 
-const grid = Array(cells).fill(null).map(() => Array(cells).fill(false))
+/**
+ *  side note: cellsVertical refers to the No of rows and cellsHorizontal refers to the No of column
+ */
+const grid = Array(cellsVertical).fill(null).map(() => Array(cellsHorizontal).fill(false))
 
-const verticals = Array(cells).fill(null).map(() => Array(cells - 1).fill(false));
+const verticals = Array(cellsVertical).fill(null).map(() => Array(cellsHorizontal - 1).fill(false));
 
-const horizontals = Array(cells - 1).fill(null).map(() => Array(cells).fill(false));
+const horizontals = Array(cellsVertical - 1).fill(null).map(() => Array(cellsHorizontal).fill(false));
 
-const startRow = Math.floor(Math.random() * cells);
+const startRow = Math.floor(Math.random() * cellsVertical);
 
-const startColumn = Math.floor(Math.random() * cells);
+const startColumn = Math.floor(Math.random() * cellsHorizontal);
 
 // /**
 //  * side note:: true in our matrix represents 'no wall'
@@ -91,7 +96,7 @@ const stepThroughCell = (row, column) => {
     const [nextRow, nextColumn, direction] = neighbor;
     
     // check if neighbor is out of bound
-    if (nextRow < 0 || nextRow >= cells || nextColumn < 0 || nextColumn >= cells) continue;
+    if (nextRow < 0 || nextRow >= cellsVertical || nextColumn < 0 || nextColumn >= cellsHorizontal) continue;
 
     // if next neighbor, continue to next neighbor
     if (grid[nextRow][nextColumn]) continue;
@@ -130,13 +135,17 @@ horizontals.forEach((row, rowIndex) => {
   row.forEach((open, columnIndex) => {
     if (open) return;
 
-    const x = (columnIndex * unitLength) + (unitLength / 2);
-    const y = (rowIndex * unitLength) + unitLength;
-    const wallWidth = unitLength;
+    const x = (columnIndex * unitLengthX) + (unitLengthX / 2);
+    const y = (rowIndex * unitLengthY) + unitLengthY;
+    const wallWidth = unitLengthX;
     const wallHeight = wallThickness;
 
     const wall = Bodies.rectangle(x, y, wallWidth, wallHeight, {
-      isStatic: true
+      label: 'wall',
+      isStatic: true,
+      render: {
+        fillStyle: 'grey'
+      }
     });
 
     World.add(world, wall)
@@ -148,13 +157,17 @@ verticals.forEach((row, rowIndex) => {
   row.forEach((open, columnIndex) => {
     if (open) return;
 
-    const x = (columnIndex * unitLength) + unitLength;
-    const y = (rowIndex * unitLength) + (unitLength / 2);
+    const x = (columnIndex * unitLengthX) + unitLengthX;
+    const y = (rowIndex * unitLengthY) + (unitLengthY / 2);
     const wallWidth = wallThickness;
-    const wallHeight = unitLength;
+    const wallHeight = unitLengthY;
     
     const wall = Bodies.rectangle(x, y, wallWidth, wallHeight, {
-      isStatic: true
+      label: 'wall',
+      isStatic: true,
+      render: {
+        fillStyle: 'grey'
+      }
     });
 
     World.add(world, wall);
@@ -164,14 +177,17 @@ verticals.forEach((row, rowIndex) => {
 
 // Goal
 
-const x = width - (unitLength / 2);
-const y = height - (unitLength / 2);
-const goalWidth = unitLength * 0.7;
-const goalHeight = unitLength * 0.7;
+const x = width - (unitLengthX / 2);
+const y = height - (unitLengthY / 2);
+const goalWidth = unitLengthX * 0.7;
+const goalHeight = unitLengthY * 0.7;
 
 const goal = Bodies.rectangle(x, y, goalWidth, goalHeight, {
   label: 'goal',
   isStatic: true,
+  render: {
+    fillStyle: 'green'
+  }
 });
 
 World.add(world, goal);
@@ -179,7 +195,13 @@ World.add(world, goal);
 
 // Ball
 
-const ball = Bodies.circle(unitLength / 2, unitLength /2, unitLength / 4, { label: 'ball' });
+const ballRadius = Math.min(unitLengthX, unitLengthY) / 4;
+const ball = Bodies.circle(unitLengthX / 2, unitLengthY /2, ballRadius, { 
+  label: 'ball',
+  render: {
+    fillStyle: 'teal'
+  } 
+});
 World.add(world, ball);
 
 // Key handling
@@ -212,8 +234,17 @@ document.addEventListener('keydown', event => {
 Events.on(engine, 'collisionStart', event => {
   event.pairs.forEach(collision => {
     const labels = ['goal', 'ball'];
+    
     if (labels.includes(collision.bodyA.label) && labels.includes(collision.bodyB.label)) {
-      console.log('user won')
+      // display winner div
+      document.querySelector('.winner').classList.remove('hidden');
+      
+      world.gravity.y = 1;
+      world.bodies.forEach(body => {
+        if (body.label === 'wall') {
+          Body.setStatic(body, false)
+        }
+      })
     }
   })
 })
